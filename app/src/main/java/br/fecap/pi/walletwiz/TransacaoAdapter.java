30db5363b1a3adbf6988b41ab8ac8754acb9e6df
IsaacMozilinha <isaac.newtonsantos2015@gmail.com>
2024-11-15 16:15:24 -0300

    Inclusão Academy

--- app/src/main/java/br/fecap/pi/walletwiz/TransacaoAdapter.java
+++ app/src/main/java/br/fecap/pi/walletwiz/TransacaoAdapter.java
@@ -2,15 +2,15 @@ package br.fecap.pi.walletwiz;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.app.AlertDialog;
-
+import androidx.recyclerview.widget.RecyclerView;
 import br.fecap.walletwiz.R;
 
 import java.util.List;
@@ -37,6 +37,13 @@ public class TransacaoAdapter extends RecyclerView.Adapter<TransacaoAdapter.View
         transacao transacao = transacoes.get(position);
         holder.textViewDescricao.setText(transacao.getData() + ": " + transacao.getTipo() + " - R$" + transacao.getValor() + " " + transacao.getCategoria());
 
+        // Define a cor do texto com base no tipo de transação
+        if ("despesa".equals(transacao.getTipo())) {
+            holder.textViewDescricao.setTextColor(Color.RED); // Vermelho para despesas
+        } else if ("receita".equals(transacao.getTipo())) {
+            holder.textViewDescricao.setTextColor(Color.parseColor("#388E3C")); // Verde escuro para receitas
+        }
+
         holder.itemView.setOnClickListener(v -> {
             Intent intent;
             if (transacao.getTipo().equals("receita")) {
@@ -49,7 +56,6 @@ public class TransacaoAdapter extends RecyclerView.Adapter<TransacaoAdapter.View
         });
 
         holder.itemView.setOnLongClickListener(v -> {
-
             AlertDialog.Builder builder = new AlertDialog.Builder(context);
             builder.setTitle("Escolha uma ação")
                     .setItems(new CharSequence[]{"Editar", "Excluir"}, (dialog, which) -> {
--- app/src/main/java/br/fecap/pi/walletwiz/perfil.java
+++ app/src/main/java/br/fecap/pi/walletwiz/perfil.java
@@ -1,16 +1,27 @@
 package br.fecap.pi.walletwiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import br.fecap.walletwiz.R;
 
 public class perfil extends AppCompatActivity {
+
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,50 @@ public class perfil extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+    }
+
+    private void setupDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        ImageView icon = findViewById(R.id.icon);
+        icon.setOnClickListener(v -> {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+            // Verificando o item selecionado e iniciando a activity correspondente
+            if (item.getItemId() == R.id.nav_home) {
+                startActivity(new Intent(perfil.this, session.class));
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.nav_acount) {
+                startActivity(new Intent(perfil.this, perfil.class));
+                return true;
+            } else if (item.getItemId() == R.id.nav_academy) {
+                startActivity(new Intent(perfil.this, academy.class));
+                return true;
+            } else if (item.getItemId() == R.id.nav_simulador_aquisicoes) {
+                startActivity(new Intent(perfil.this, simulador_aquisicoes.class));
+                return true;
+            }else if (item.getItemId() == R.id.nav_extract) {
+                startActivity(new Intent(perfil.this, extrato.class));
+                return true;
+            }
+
+            return false;
         });
     }
 }
