e92559867d76627888a78266e0da5412a2e14ce1
Giovanne Braga <gi.braga24@hotmail.com>
2024-11-18 19:03:53 -0300

    add transactions endpoint

--- src/Backend/db/seeds.rb
+++ src/Backend/db/seeds.rb
@@ -4,78 +4,74 @@
 #
 # Example:
 #
-    [
-        {
-            "nome": "Giovanne",
-            "sobrenome": "Braga",
-            "cpf": "41581511515",
-            "endereco": "Tuiuti",
-            "email": "Teste",
-            "senha": "123"
-        },
-        {
-            "nome": "Isaac",
-            "sobrenome": "Newton",
-            "cpf": "1451561565",
-            "endereco": "zona sul",
-            "email": "Teste1",
-            "senha": "123"
-        },
-        {
-            "nome": "Icaro",
-            "sobrenome": "Palmeiras",
-            "cpf": "474747477",
-            "endereco": "Fecap",
-            "email": "Teste3",
-            "senha": "123"
-        }
-        
-    ].each do |user|
-     User.find_or_create_by!(user)
-     end
+[
+    {
+        "nome": "Giovanne",
+        "sobrenome": "Braga",
+        "cpf": "41581511515",
+        "endereco": "Tuiuti",
+        "email": "Teste",
+        "senha": "123"
+    },
+    {
+        "nome": "Isaac",
+        "sobrenome": "Newton",
+        "cpf": "1451561565",
+        "endereco": "zona sul",
+        "email": "Teste1",
+        "senha": "123"
+    },
+    {
+        "nome": "Icaro",
+        "sobrenome": "Palmeiras",
+        "cpf": "474747477",
+        "endereco": "Fecap",
+        "email": "Teste3",
+        "senha": "123"
+    }
+].each do |user|
+    User.find_or_create_by!(user)
+    end
 
-     [
-        {
-            "nome": "Aluguel",
-            "user_id": 1
-        },
-        {
-            "nome": "Mercado",
-            "user_id": 1
-        },
-        {
-            "nome": "Luz",
-            "user_id": 2
-        }
-        
-    ].each do |transaction_type_id|
-     TransactionType.find_or_create_by!(transaction_type_id)
-     end
+[
+    {
+        "nome": "Aluguel",
+        "user_id": 1
+    },
+    {
+        "nome": "Mercado",
+        "user_id": 1
+    },
+    {
+        "nome": "Luz",
+        "user_id": 1
+    }
+].each do |transaction_type_id|
+    TransactionType.find_or_create_by!(transaction_type_id)
+    end
 
-
-     [
-        {
-            "valor": 3000,
-            "data": "24/11/2004",
-            "observacao": "teste",
-            "transaction_type_id": 2,
-            "user_id": 2
-        },
-        {
-            "valor": 5000,
-            "data": "11/10/2023",
-            "observacao": "teste",
-            "transaction_type_id": 2,
-            "user_id": 2
-        },
-        {
-            "valor": -3000,
-            "data": "11/11/2011",
-            "observacao": "teste",
-            "transaction_type_id": 2,
-            "user_id": 2
-        }
-        
-    ].each do |transaction|
-     Transaction.find_or_create_by!(transaction)
-     end
+[
+    {
+        "valor": 3000,
+        "data": "24/11/2004",
+        "observacao": "teste",
+        "transaction_type_id": 3,
+        "user_id": 1
+    },
+    {
+        "valor": 5000,
+        "data": "11/10/2023",
+        "observacao": "teste",
+        "transaction_type_id": 2,
+        "user_id": 1
+    },
+    {
+        "valor": -3000,
+        "data": "11/11/2011",
+        "observacao": "teste",
+        "transaction_type_id": 1,
+        "user_id": 1
+    }
+].each do |transaction|
+    Transaction.find_or_create_by!(transaction)
+    end
--- src/Backend/test/models/transaction_test.rb
+++ src/Backend/test/models/transaction_test.rb
@@ -10,6 +10,7 @@
 #  user_id             :bigint           not null
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
+#  nome                :string
 #
 require "test_helper"
 
