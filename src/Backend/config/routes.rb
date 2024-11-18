192c77497c95d9a3e4afbd3140222373e08a37ca
Giovanne Braga <gi.braga24@hotmail.com>
2024-11-18 14:00:52 -0300

    add login endpoint

--- src/Backend/config/routes.rb
+++ src/Backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "users/:id/show_balance", controller: "users", action: :show_balance
+  post "users/login", controller: "users", action: :login
 
   resources :transactions
   resources :transaction_types
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  
+
 
   # Defines the root path route ("/")
   # root "posts#index"
